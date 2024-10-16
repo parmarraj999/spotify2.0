40a2b7f14aaef3a2ff15316a8a9a3719f057d466
parmarraj999 <rajraja5122@gmail.com>
2024-10-16 22:46:50 +0530

    album on search done

--- src/page/search/resultComponent/albumList.js
+++ src/page/search/resultComponent/albumList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function AlbumList({data}) {
+function AlbumList({ data }) {
 
   console.log(data)
 
@@ -9,13 +9,16 @@ function AlbumList({data}) {
       <h3>Albums</h3>
       <div className='list_container' >
         {
-          data.map((data)=>{
-            return(
+          data.map((data) => {
+            return (
               <div className='search_album_cover' >
-                <div className='search_album_img' >
-                  <img src={data.images[0].url} />
+                <div className='search_album_img' style={{backgroundImage:`url(${data.images[0].url})`}}>
+                  {/* <img className='img_1' src={data.images[0].url} /> */}
+                  <div>
+                    <h3>{data.name}</h3>
+                    <h4>{data.artists[0].name},</h4>
+                  </div>
                 </div>
-                <h3>{data.name}</h3>
               </div>
             )
           })
